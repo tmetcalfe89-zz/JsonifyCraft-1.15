c9e00661700804c27ab6124cb0393222c57a4ad1
Tim <tmetcalfe89@gmail.com>
2020-03-17 00:37:28 -0700

    Added lore
    To add lore to an item, add the lore flag, then include an entry in your lang file with item.jsonifycraft.itemname.lore as the key.

--- src/main/java/us/timinc/jsonifycraft/world/JsonedItem.java
+++ src/main/java/us/timinc/jsonifycraft/world/JsonedItem.java
@@ -1,8 +1,15 @@
 package us.timinc.jsonifycraft.world;
 
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
 import us.timinc.jsonifycraft.description.ItemDescription;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class JsonedItem extends Item {
     private final ItemDescription description;
@@ -15,5 +22,11 @@ public class JsonedItem extends Item {
     @Override
     public boolean hasEffect(ItemStack stack) {
         return stack.getItem() == this && description.hasFlag("shimmer");
+    }
+
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+        if (description.hasFlag("lore")) {
+            tooltip.add(new TranslationTextComponent(String.format("item.jsonifycraft.%s.lore", description.name)));
+        }
     }
 }
